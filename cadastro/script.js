a993d9ec6f1b4bf8ee434ebda34c0d4ae9ca72a3
Base-box <report.thgx@gmail.com>
2023-07-28 19:24:29 -0300

    modific
    alterações de input com novo estilo, (pendente)

---
+++ cadastro/script.js
@@ -0,0 +1,12 @@
+
+function hide(){
+    var campo__fora = document.getElementById('cF').style.display = "none";
+   
+}
+function hideoF(){
+    var campo__fora = document.getElementById('cF').style.display = "block";
+    
+
+
+}//bottom de hide login
+
