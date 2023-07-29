863eaed91acdbc319db19cca5c10a5724ca8cb81
Base-box <report.thgx@gmail.com>
2023-07-28 21:01:23 -0300

    y

--- cadastro/script.js
+++ cadastro/script.js
@@ -1,12 +1,1 @@
 
-function hide(){
-    var campo__fora = document.getElementById('cF').style.display = "none";
-   
-}
-function hideoF(){
-    var campo__fora = document.getElementById('cF').style.display = "block";
-    
-
-
-}//bottom de hide login
-
